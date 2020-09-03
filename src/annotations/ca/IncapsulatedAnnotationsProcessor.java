60c3e096d984d6b37f8c49f937b95cd461a8bf06
vbalan <vbalan@patriot.ambianta.local>
2020-09-03 12:21:06 +0300

    123132132132

--- src/annotations/ca/IncapsulatedAnnotationsProcessor.java
+++ src/annotations/ca/IncapsulatedAnnotationsProcessor.java
@@ -1,10 +1,12 @@
 package annotations.ca;
 
+import java.util.List;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
@@ -18,15 +20,37 @@ public class IncapsulatedAnnotationsProcessor extends AbstractProcessor {
 		
 		for(var e : roundEnv.getElementsAnnotatedWith(Incapsulated.class)) {
 			
-			for(var ee : e.getEnclosedElements()) {
+			List<? extends Element> enclosedElements = e.getEnclosedElements();
+			
+			for(var ee : enclosedElements) {
 				if(ee.getKind() == ElementKind.FIELD) {
-					if(ee.getModifiers().contains(Modifier.PUBLIC)) {
-						processingEnv.getMessager().printMessage(Kind.ERROR, "Ooops! That class contains public FIELDS", ee);
+					if(ee.getModifiers().isEmpty()) {
+						processingEnv.getMessager().printMessage(Kind.WARNING, "Ooops! This FIELD is has no set modifier", ee);
+					}
+					else if(ee.getModifiers().contains(Modifier.PUBLIC) && !ee.getModifiers().contains(Modifier.FINAL)) {
+						processingEnv.getMessager().printMessage(Kind.WARNING, "Ooops! This FIELD is not FINAL and set to PUBLIC modifier", ee);
+					}
+					else{
+						int increment = 0;
+						for(var m : enclosedElements) {
+							if(m.getKind() == ElementKind.METHOD) {
+								if(m.getModifiers().contains(Modifier.PRIVATE) && (m.getSimpleName().toString().startsWith("set")
+									|| m.getSimpleName().toString().startsWith("get"))) {
+									processingEnv.getMessager().printMessage(Kind.WARNING, "Ooops! This METHOD is set to PRIVATE modifier", m);
+								}
+								else if((m.getSimpleName().toString().startsWith("set")
+										|| m.getSimpleName().toString().startsWith("get"))
+										&& m.getSimpleName().toString().toLowerCase().contains(ee.getSimpleName().toString().toLowerCase())){
+									increment++;
+								}
+							}
+						}
+						if(increment < 2) {
+							processingEnv.getMessager().printMessage(Kind.WARNING, "Ooops! This FIELD is has no set GETTER OR SETTER", ee);
+						}
 					}
 				}
 			}
-			
-			
 		}
 					
 		return true;
