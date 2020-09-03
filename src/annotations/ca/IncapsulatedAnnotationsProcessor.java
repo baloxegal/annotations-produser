8b30a91722c138ec3a9ca39e9d54cb989b5f646d
Your Name <you@example.com>
2020-09-03 20:01:41 +0300

    with incapsulating warnings

--- src/annotations/ca/IncapsulatedAnnotationsProcessor.java
+++ src/annotations/ca/IncapsulatedAnnotationsProcessor.java
@@ -14,46 +14,71 @@ import javax.tools.Diagnostic.Kind;
 
 @SupportedAnnotationTypes("annotations.ca.Incapsulated")
 public class IncapsulatedAnnotationsProcessor extends AbstractProcessor {
-
+	
 	@Override
 	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-		
+				
 		for(var e : roundEnv.getElementsAnnotatedWith(Incapsulated.class)) {
 			
 			List<? extends Element> enclosedElements = e.getEnclosedElements();
+						
+			int incrementConstructor = 0;
 			
 			for(var ee : enclosedElements) {
-				if(ee.getKind() == ElementKind.FIELD) {
+				
+				if(ee.getKind().equals(ElementKind.CONSTRUCTOR)){
+					incrementConstructor++;
+				}
+				
+				else if(ee.getKind().equals(ElementKind.FIELD)) {
+					
 					if(ee.getModifiers().isEmpty()) {
-						processingEnv.getMessager().printMessage(Kind.WARNING, "Ooops! This FIELD is has no set modifier", ee);
+						processingEnv.getMessager().printMessage(Kind.WARNING, "Ooops! This FIELD has no set modifier", ee);
 					}
-					else if(ee.getModifiers().contains(Modifier.PUBLIC) && !ee.getModifiers().contains(Modifier.FINAL)) {
+					
+					if(ee.getModifiers().contains(Modifier.PUBLIC) && !ee.getModifiers().contains(Modifier.FINAL)) {
 						processingEnv.getMessager().printMessage(Kind.WARNING, "Ooops! This FIELD is not FINAL and set to PUBLIC modifier", ee);
 					}
-					else{
-						int increment = 0;
+					
+					else if(ee.getModifiers().contains(Modifier.PRIVATE) || ee.getModifiers().contains(Modifier.PROTECTED)
+							|| ee.getModifiers().isEmpty()){
+						
+						int incrementGet = 0, incrementSet = 0;
+						
 						for(var m : enclosedElements) {
-							if(m.getKind() == ElementKind.METHOD) {
+							if(m.getKind().equals(ElementKind.METHOD)) {
+								
 								if(m.getModifiers().contains(Modifier.PRIVATE) && (m.getSimpleName().toString().startsWith("set")
 									|| m.getSimpleName().toString().startsWith("get"))) {
 									processingEnv.getMessager().printMessage(Kind.WARNING, "Ooops! This METHOD is set to PRIVATE modifier", m);
 								}
-								else if((m.getSimpleName().toString().startsWith("set")
-										|| m.getSimpleName().toString().startsWith("get"))
-										&& m.getSimpleName().toString().toLowerCase().contains(ee.getSimpleName().toString().toLowerCase())){
-									increment++;
+								
+								if(m.getSimpleName().toString().equalsIgnoreCase("get".concat(ee.getSimpleName().toString()))){
+									incrementGet++;
+								}
+								
+								else if(m.getSimpleName().toString().equalsIgnoreCase("set".concat(ee.getSimpleName().toString()))){
+									incrementSet++;
 								}
 							}
 						}
-						if(increment < 2) {
-							processingEnv.getMessager().printMessage(Kind.WARNING, "Ooops! This FIELD is has no set GETTER OR SETTER", ee);
+						
+						if(incrementGet == 0) {
+							processingEnv.getMessager().printMessage(Kind.WARNING, "Ooops! This FIELD has no set GETTER", ee);
 						}
+						
+						if(incrementSet == 0) {
+							processingEnv.getMessager().printMessage(Kind.WARNING, "Ooops! This FIELD has no set SETTER", ee);
+						}						
 					}
 				}
+			}
+			
+			if(incrementConstructor == 0){
+				processingEnv.getMessager().printMessage(Kind.WARNING, "Ooops! This CLASS has no CONSTRUCTOR");
 			}
 		}
 					
 		return true;
 	}
-
 }
