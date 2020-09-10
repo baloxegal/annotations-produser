91fcdc4fddf6b243fee6934ede167509d7f4638e
Your Name <you@example.com>
2020-09-10 21:54:51 +0300

    45646545646

--- src/annotations/ca/IncapsulatedAnnotationsProcessor.java
+++ src/annotations/ca/IncapsulatedAnnotationsProcessor.java
@@ -2,6 +2,7 @@ package annotations.ca;
 
 import java.util.Map;
 import java.util.HashMap;
+import java.lang.annotation.Annotation;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -9,10 +10,14 @@ import java.util.Set;
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeKind;
+import javax.lang.model.type.TypeMirror;
+import javax.lang.model.type.TypeVisitor;
 import javax.tools.Diagnostic.Kind;
 
 @SupportedAnnotationTypes("annotations.ca.Incapsulated")
@@ -30,6 +35,8 @@ public class IncapsulatedAnnotationsProcessor extends AbstractProcessor {
 			if(!enclosedElementsKinds.containsValue(ElementKind.CONSTRUCTOR)) {
 				processingEnv.getMessager().printMessage(Kind.WARNING, "Ooops! This CLASS has no CONSTRUCTOR", e);	
 			}
+			
+			int incrementConstructorParameters = 0;
 									
 			for(var ee : enclosedElementsKinds.keySet()) {
 				
@@ -76,15 +83,57 @@ public class IncapsulatedAnnotationsProcessor extends AbstractProcessor {
 					}
 				}
 				else if(enclosedElementsKinds.get(ee).equals(ElementKind.CONSTRUCTOR)) {
+										
+					processingEnv.getMessager().printMessage(Kind.WARNING, "Ooops!" + ee.getClass().getSimpleName(), e);
 					
 					List<ElementKind> constructorElementsKind = new ArrayList<ElementKind>();
 					ee.getEnclosedElements().forEach(ek -> constructorElementsKind.add(ek.getKind()));
-					
-					if(!constructorElementsKind.contains(ElementKind.PARAMETER)) {
-						processingEnv.getMessager().printMessage(Kind.WARNING, "Ooops! This CLASS has no CONSTRUCTOR with PARAMETERS", e);
-					}
+										
+					if(constructorElementsKind.contains(ElementKind.PARAMETER)) {
+						incrementConstructorParameters++;
+						processingEnv.getMessager().printMessage(Kind.WARNING, "WOW!", e);
+					}					
 				}
+				processingEnv.getMessager().printMessage(Kind.WARNING, "--!" + ee.getClass().getSimpleName(), e);
+			}			
+			if(incrementConstructorParameters == 0) {
+				processingEnv.getMessager().printMessage(Kind.WARNING, "Ooops! This CLASS has no CONSTRUCTOR with PARAMETERS", e);
 			}
+			else
+				processingEnv.getMessager().printMessage(Kind.WARNING, "Booo!", e);
+			TypeVisitor<enclosedElementsKinds, P> vb = new TypeMirror() {
+				
+				@Override
+				public <A extends Annotation> A[] getAnnotationsByType(Class<A> annotationType) {
+					// TODO Auto-generated method stub
+					return null;
+				}
+				
+				@Override
+				public List<? extends AnnotationMirror> getAnnotationMirrors() {
+					// TODO Auto-generated method stub
+					return null;
+				}
+				
+				@Override
+				public <A extends Annotation> A getAnnotation(Class<A> annotationType) {
+					// TODO Auto-generated method stub
+					return null;
+				}
+				
+				@Override
+				public TypeKind getKind() {
+					// TODO Auto-generated method stub
+					return null;
+				}
+				
+				@Override
+				public <R, P> R accept(TypeVisitor<R, P> v, P p) {
+					// TODO Auto-generated method stub
+					return null;
+				}
+			};
+			processingEnv.getMessager().printMessage(Kind.WARNING, "++!", e);
 		}
 					
 		return true;
