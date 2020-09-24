302bf808d3b34e5ca895601a351f2f1552fb8fb2
Your Name <you@example.com>
2020-09-24 21:08:02 +0300

    74123

--- src/instrumentation/ClassTransformer.java
+++ src/instrumentation/ClassTransformer.java
@@ -3,55 +3,60 @@ package instrumentation;
 import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtConstructor;
-import javassist.CtField;
 import javassist.CtNewConstructor;
+import javassist.CtField;
+
+import java.lang.reflect.Modifier;
+
+import annotations.ca.AddConstructor;
 
 import javassist.NotFoundException;
-import annotations.ca.AddConstructor;
 import javassist.CannotCompileException;
 
 public class ClassTransformer {
 	
 	public static Class<?> transform(String className) throws NotFoundException, CannotCompileException {
 		
-		System.out.println("STARTING CLASS TRANSFORMATION - Premain");
+		System.out.println("STARTING CLASS TRANSFORMATION");
 		
 		ClassPool pool = ClassPool.getDefault();
 		
 		CtClass cc = pool.get(className);
 		
-//		if(!cc.getPackageName().equalsIgnoreCase("domain") || cc.isInterface() || !cc.hasAnnotation(AddConstructor.class)){
-//			return cc.toClass();
-//		}
-		
-//		CtConstructor ccons = cc.getDeclaredConstructor(null);
-//		cc.removeConstructor(ccons);
-//		
-//		CtConstructor cstructor = CtNewConstructor.defaultConstructor(cc);
-//			
-//		cstructor.setBody("this.age = Integer.valueOf(50);");
-//		cc.addConstructor(cstructor);
-		
-		CtField[] cf = cc.getDeclaredFields();
-		CtField[] cfm = cc.getDeclaredFields();
-		CtClass[] cft = new CtClass[cf.length];
-		
-		for(int i = 0, j = 0; i < cf.length; i++) {
-			if(cf[i].getModifiers() != 1) {
-				cfm[j] = cf[i];
-				cft[j] = cf[i].getType();
-				j++;
-			}
+		if(!cc.getPackageName().equalsIgnoreCase("domain") || cc.isInterface() || !cc.hasAnnotation(AddConstructor.class)){
+			return cc.toClass();
 		}
 		
-//		CtConstructor ccons = CtNewConstructor.make("public Person (Integer age, Boolean isBuyer)", cc);
-//		ccons.setBody("this.age = age; this.isBuyer = isBuyer;");
+		CtConstructor cconsDef = cc.getDeclaredConstructor(null);
+		cc.removeConstructor(cconsDef);
 		
-		CtConstructor ccons = CtNewConstructor.make("Person(Integer age, Boolean isBuyer){this.age = age; this.isBuyer = isBuyer;}", cc);
+		CtConstructor cstructor = CtNewConstructor.defaultConstructor(cc);
+			
+		cstructor.setBody("this.age = Integer.valueOf(70);");
+		cc.addConstructor(cstructor);
+		
+//		CtField[] cf = cc.getDeclaredFields();
+//		String[] cfm = new String[cf.length];
+//		CtClass[] cft = new CtClass[cf.length];
+//		
+//		for(int i = 0, j = 0; i < cf.length; i++) {
+//			if(cf[i].getModifiers() == Modifier.PRIVATE) {
+//				cfm[j] = cf[i].getName();
+//				cft[j] = cf[i].getType();
+//				j++;
+//			}
+//		}		
+
+		CtConstructor ccons = CtNewConstructor.make("public Person(Integer age, Boolean isBuyer){this.age = age; this.isBuyer = isBuyer;}", cc);
+		
+//		CtConstructor ccons = CtNewConstructor.make("public " + cc.getSimpleName()
+//								+ "(" + cft[0] + " " + cfm[0] + ", " + cft[1] + " " + cfm[1]
+//								+ ") {this." + cfm[0] + " = " + cfm[0] + "; "
+//								+ "this." + cfm[1] + " = " + cfm[1] + ";}", cc);
 		
 		cc.addConstructor(ccons);
 		
-		System.out.println("ENDING CLASS TRANSFORMATION - Premain");		
+		System.out.println("ENDING CLASS TRANSFORMATION");		
 	
 		return cc.toClass();
 	}
--- src/instrumentation/TestAgent.java
+++ src/instrumentation/TestAgent.java
@@ -4,9 +4,6 @@ import java.lang.instrument.Instrumentation;
 import java.lang.instrument.ClassFileTransformer;
 import java.security.ProtectionDomain;
 
-import javassist.CannotCompileException;
-import javassist.NotFoundException;
-
 public class TestAgent {
 	
 	public static void premain(String args, Instrumentation instrumentation) {
@@ -23,23 +20,12 @@ public class TestAgent {
 									ProtectionDomain domain,
 									byte[] buffer) {
 								
-				System.out.println("Class was loaded: " + name);
-				System.out.println("Loader used: " + loader + "\n");
+//				System.out.println("Class was loaded: " + name);
+//				System.out.println("Loader used: " + loader + "\n");
 				
-				if(name == "domain.Person")
-					try {
-						ClassTransformer.transform(name);
-					} catch (NotFoundException e) {
-						e.printStackTrace();
-					} catch (CannotCompileException e) {
-						e.printStackTrace();
-					}
 				return null;
 			}
 						
 		});
-		
-		
-
 	}
 }
